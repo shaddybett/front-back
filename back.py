dac0346f0bcc1a203195c72c6de6d88509a2e067
Shaddy <shadybett540@gmail.com>
2023-12-26 15:49:26 +0300

    added new sample to database

--- back.py
+++ back.py
@@ -40,7 +40,7 @@ Session = sessionmaker(bind=engine)
 session = Session()   
 
 # Example: Add a new pet
-petName = 'Amber'
+petName = 'Kai'
 petBreed = 'German Shepherd'
 petAge = 12
 add_pet(session, petName, petBreed, petAge)
